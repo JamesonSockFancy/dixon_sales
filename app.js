55824079e9070edfc77be7321a9e8ca3d4bb8818
Jameson <jameson@sockfancy.com>
2018-12-15 23:34:52 -0500

    app reconfig

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 var express = require('express');
 var app = express();
-var mysql = require('mysql');
 var bodyParser = require('body-parser')
 
 app.set("view engine", "ejs");
@@ -9,23 +8,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-      res.render("home");
-    });
 
+    res.render("home");
+});
 
-// const port=process.env.PORT || 3000
-// const server = http.createServer((req, res) => {
-
-//     res.statusCode = 200;
-
-//     res.setHeader('Content-Type', 'text/html');
-
-//     res.end('<h1>Hello World</h1>');
-
-// });
-
-// server.listen(port,() => {
-
-//     console.log(`Server running at port `+port);
-
-// });
+app.listen(8080, function () {
+    console.log('App listening on port 8080!');
+});
