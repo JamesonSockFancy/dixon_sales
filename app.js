cd2d0fbf46d988df52d38b14dee0a96b73bd2cbc
Jameson <jameson@sockfancy.com>
2018-12-16 00:22:40 -0500

    rework the app

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
-const http = require('http');
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname + "/public"));
-
-const http = require('http');
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
 });
