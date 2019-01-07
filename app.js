f7fbe1cba61d52227e8995722c3fba23007d26d0
Jameson <jameson@sockfancy.com>
2019-01-06 20:23:35 -0500

    node shell app update

--- app.js
+++ app.js
@@ -1,193 +1,25 @@
 const express = require('express');
-const http = require('http');
-const app = express();
+const bodyParser = require('body-parser')
 const path = require('path');
-var request = require('request-promise');
-var qs = require('qs');
-var parseString = require('xml2js').parseString;
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    var getOrders = {
-  
-              method: 'GET',
-              url: 'https://api.squarespace.com/1.0/commerce/orders',
-              headers:
-                {
-                  'Authorization': 'Bearer 63780558-bd3a-44ca-9b54-66cc777abfe7',
-                  'Content-Type': 'application/json',
-                  'User-Agent': 'Mozilla/5.0'
-                },
-                rejectUnauthorized: false
-            };
-            
-            var getAuth = {
-              
-              method: 'POST',
-              body: qs.stringify({
-               'refresh_token': 'ad801fd268d63e30299c28f257c2bec7d32dd2a8',
-               'client_secret': 'c5e4af85bda04d99e69c8121bb52866407d580e07fba7ca8a7cc2798e63f6aae',
-               'client_id': '1d9252b179e2180935270c3f66ccac118a9f81142c616a5faa886ee6fa3c5422',
-               'grant_type': 'refresh_token',
-             }),
-              headers:
-                {
-                  'Content-type': "application/x-www-form-urlencoded"
-                },
-              url: 'https://cloud.merchantos.com/oauth/access_token.php'
-             
-             
-             };
-            
-            let orders; 
-            request(getOrders)
-             .then(ordersResponse => {
-              var orders = JSON.parse(ordersResponse)
-              orders.result.forEach((order)=> {
-                // console.log(order.id);
-                if (order.fulfillmentStatus == 'FULFILLED') {
-                  // console.log(order.fulfillmentStatus);
-                  var sale = {
-                    "employeeID": 15,
-                    "registerID": 2,
-                    "shopID": 1,
-                    "customerID": Math.floor(Math.random() * 10000),
-                    "completed": true,
-                    "SaleLines": {
-                        "SaleLine": [
-                        {
-                            "itemID": order.lineItems[0].sku,
-                            "unitQuantity": order.lineItems[0].quantity
-                        },
-                        ]
-                    },
-                    "SalePayments": {
-                      "SalePayment": {
-                        "amount": order.grandTotal.value,
-                        "paymentTypeID": 3
-                      }
-                    }
-                  }
-                }
-            
-                console.log(sale);
-                
-              })
-            });
-            res.write('<html>');
-            res.write('<head><title>Enter Message</title><head>');
-            res.write('<body><p>sale</p></body>');
-            res.write('<script> document.write(sale) </script>');
-            res.write('</html>');        
-              
-    return res.end();
-  }
-  
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'));
 });
 
-server.listen(3000);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// app.set("view engine", "ejs");
-// app.use(express.static(__dirname + "/public"));
-
-// app.get('/', function(req, res) {
-//     var getOrders = {
-  
-//         method: 'GET',
-//         url: 'https://api.squarespace.com/1.0/commerce/orders',
-//         headers:
-//           {
-//             'Authorization': 'Bearer 63780558-bd3a-44ca-9b54-66cc777abfe7',
-//             'Content-Type': 'application/json',
-//             'User-Agent': 'Mozilla/5.0'
-//           },
-//           rejectUnauthorized: false
-//       };
-      
-//       var getAuth = {
-        
-//         method: 'POST',
-//         body: qs.stringify({
-//          'refresh_token': 'ad801fd268d63e30299c28f257c2bec7d32dd2a8',
-//          'client_secret': 'c5e4af85bda04d99e69c8121bb52866407d580e07fba7ca8a7cc2798e63f6aae',
-//          'client_id': '1d9252b179e2180935270c3f66ccac118a9f81142c616a5faa886ee6fa3c5422',
-//          'grant_type': 'refresh_token',
-//        }),
-//         headers:
-//           {
-//             'Content-type': "application/x-www-form-urlencoded"
-//           },
-//         url: 'https://cloud.merchantos.com/oauth/access_token.php'
-       
-       
-//        };
-      
-//       let orders; 
-//       request(getOrders)
-//        .then(ordersResponse => {
-//         var orders = JSON.parse(ordersResponse)
-//         orders.result.forEach((order)=> {
-//           // console.log(order.id);
-//           if (order.fulfillmentStatus == 'FULFILLED') {
-//             // console.log(order.fulfillmentStatus);
-//             var sale = {
-//               "employeeID": 15,
-//               "registerID": 2,
-//               "shopID": 1,
-//               "customerID": Math.floor(Math.random() * 10000),
-//               "completed": true,
-//               "SaleLines": {
-//                   "SaleLine": [
-//                   {
-//                       "itemID": order.lineItems[0].sku,
-//                       "unitQuantity": order.lineItems[0].quantity
-//                   },
-//                   ]
-//               },
-//               "SalePayments": {
-//                 "SalePayment": {
-//                   "amount": order.grandTotal.value,
-//                   "paymentTypeID": 3
-//                 }
-//               }
-//             }
-//           }
-      
-//           console.log(sale);
-          
-//         })
-      
-      
-//        });
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
-
-// app.listen(process.env.PORT || 4000, function(){
-//     console.log('Your node js server is running');
-// });
-
-
-
+app.listen(3000);
