ea9d37cc7910c350b60ef8808cf598787c727503
Jameson <jameson@sockfancy.com>
2019-01-29 22:40:47 -0500

    add controller

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 
-
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -11,7 +11,6 @@ app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
 
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,7 +22,6 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'));
 });
-
 
 
 app.listen(process.env.PORT || 4000, function(){
